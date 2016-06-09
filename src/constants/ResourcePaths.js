92b7bdcf17091a5512dc562232a85dee33158c2a
dk\anb <anb@sitecore.net>
2016-06-09 11:34:24 +0200

    fixed controllers link

--- src/constants/ResourcePaths.js
+++ src/constants/ResourcePaths.js
@@ -1,7 +1,7 @@
 const CONFIG_PATH = process.env.RESOURCE_PATH_PREFIX;
 
-export const RESOURCE_PATH_CAMPAIGNS = `${CONFIG_PATH}/campaigns`;
+export const RESOURCE_PATH_CAMPAIGNS = `${CONFIG_PATH}/-/Campaigns`;
 export const RESOURCE_PATH_DASHBOARD = `${CONFIG_PATH}/-/Dashboard`;
-export const RESOURCE_PATH_LANDINGPAGES = `${CONFIG_PATH}/landingpages`;
-export const RESOURCE_PATH_OUTCOMES = `${CONFIG_PATH}/outcomes`;
+export const RESOURCE_PATH_LANDINGPAGES = `${CONFIG_PATH}/-/Landingpages`;
+export const RESOURCE_PATH_OUTCOMES = `${CONFIG_PATH}/-/Outcomes`;
 export const RESOURCE_PATH_PROCESS = '/api/xgen/jobs';
